ae9ad8cf0870d987c7b4d10cd3d7439928b43e50
FidanKerimova56 <Fidan.kerimova56@gmail.com>
2022-03-14 03:51:33 -0700

    first js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let input = prompt("add two number");
+
+let seperate_nums= input.split(" ");
+let num1 = seperate_nums[1];
+let num2 = seperate_nums[0];
+
+console.log(`${num1} is first num`);
+console.log(`${num2} is first num`);
